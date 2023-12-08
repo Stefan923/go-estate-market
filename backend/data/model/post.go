d754622455c6861c802c45221bedae420ded21c9
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2023-12-08 16:34:35 +0200

    Added models related to announcements for properties

---
+++ backend/data/model/post.go
@@ -0,0 +1,16 @@
+package model
+
+type Post struct {
+	BaseModel
+	Title        string `gorm:"type:string; size:64; not null;"`
+	Description  string `gorm:"type:string; size:1024; not null;"`
+	PostComments *[]PostComment
+	PropertyId   uint `gorm:"unique;"`
+}
+
+type PostComment struct {
+	BaseModel
+	Description string `gorm:"type:string; size:512; not null;"`
+	PostId      uint
+	Post        Post `gorm:"foreignKey:PostId; constraint:OnUpdate:NO ACTION; OnDelete:NO ACTION;"`
+}
